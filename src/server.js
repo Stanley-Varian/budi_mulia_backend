1e33d54a58c566433b9fc076d880b938d88e5da1
Stanley-Varian <stanleyvarian@example.com>
2026-06-14 21:18:19 +0700

    update

--- src/server.js
+++ src/server.js
@@ -2,34 +2,37 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import authRoutes   from "./routes/auth.routes.js";
-import siswaRoutes  from "./routes/siswa.routes.js";
-import guruRoutes   from "./routes/guru.routes.js";
-import adminRoutes  from "./routes/admin.routes.js";
+import authRoutes  from "./routes/auth.routes.js";
+import siswaRoutes from "./routes/siswa.routes.js";
+import guruRoutes  from "./routes/guru.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors({
   origin: [
     "http://localhost:3000",
     "https://budi-mulia-rho.vercel.app",
-    /https:\/\/.*\.vercel\.app$/  // allow semua *.vercel.app
+    /https:\/\/.*\.vercel\.app$/
   ],
   credentials: true
 }));
 app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// ── Routes ──
 app.use("/api/auth",  authRoutes);
 app.use("/api/siswa", siswaRoutes);
 app.use("/api/guru",  guruRoutes);
 app.use("/api/admin", adminRoutes);
 
-// Health check
 app.get("/", (req, res) => res.json({ message: "API SMA Budi Mulia berjalan ✅" }));
 
 const PORT = process.env.PORT || 5000;
